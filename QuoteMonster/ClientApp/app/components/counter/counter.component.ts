4886d9b337c4de4c52f58d5bf88fda3c5abe591d
Pj <petejw@theswamp.co.uk>
2017-04-11 07:43:38 +0100

    Tutorial: Add a simple service and consume. - Add HelloService

--- QuoteMonster/ClientApp/app/components/counter/counter.component.ts
+++ QuoteMonster/ClientApp/app/components/counter/counter.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { HelloService } from '../helloworld.service';
 
 @Component({
     selector: 'counter',
-    templateUrl: './counter.component.html'
+	templateUrl: './counter.component.html',
+	providers: [HelloService]
 })
-export class CounterComponent {
-    public currentCount = 0;
+export class CounterComponent implements OnInit {
+	public currentCount = 0;
+	public greeting = '';
+
+	constructor(private helloService: HelloService) { }
+
+	ngOnInit(): void {
+		this.helloService.getGreetingSlowly()
+			.then(g => this.greeting = g);
+
+		this.helloService.getGreeting()
+			.then(g => this.greeting = g);
+	}
 
     public incrementCounter() {
         this.currentCount++;
---
+++ QuoteMonster/ClientApp/app/components/helloworld.service.ts
@@ -0,0 +1,17 @@
+﻿import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HelloService {
+
+	getGreeting(): Promise<string> {
+		return Promise.resolve('Hello, world!');
+	}
+
+	getGreetingSlowly(): Promise<string> {
+
+		return new Promise(r => {
+			setTimeout(() => r('Hello, slowly!'), 5000);
+		});
+
+	}
+}
