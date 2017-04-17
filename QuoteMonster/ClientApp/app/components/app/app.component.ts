3d0f8a66114dd5942bb3adb99adefe9d60192725
Pj <petejw@theswamp.co.uk>
2017-04-17 11:55:11 +0100

    Add Auth0 stuff

--- QuoteMonster/ClientApp/app/components/app/app.component.ts
+++ QuoteMonster/ClientApp/app/components/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../services/auth.service';
 
 @Component({
     selector: 'app',
+	providers: [Auth],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	constructor(private auth: Auth) { }
 }
--- QuoteMonster/ClientApp/app/components/navmenu/navmenu.component.ts
+++ QuoteMonster/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../services/auth.service';
 
 @Component({
     selector: 'nav-menu',
+	providers: [Auth],
     templateUrl: './navmenu.component.html',
     styleUrls: ['./navmenu.component.css']
 })
 export class NavMenuComponent {
+	constructor(private auth: Auth) { }
 }
