bc21d7d72679e36591347541abbf6a333492cbf7
Pj <petejw@theswamp.co.uk>
2017-04-23 20:30:16 +0100

    ..

--- QuoteMonster/Model/Quote.cs
+++ QuoteMonster/Model/Quote.cs
@@ -11,7 +11,6 @@ namespace QuoteMonster.Model
 		public string Text { get; set; }
 		public DateTime CreatedOn { get; set; }
 		public DateTime? ModifiedOn { get; set; }
-
 		public Guid CreatedBy { get; set; }
 		public Guid? ModifiedBy { get; set; }
 	}
