8d50cab293e9acb9aa18fa7235c67c2485bb8ff0
Pj <petejw@theswamp.co.uk>
2017-04-29 11:25:49 +0100

    Swamp out Http service for our own authenticated version

---
+++ QuoteMonster/ClientApp/app/services/authenticated-http.service.ts
@@ -0,0 +1,45 @@
+﻿import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+
+@Injectable()
+export class AuthenticatedHttp {
+
+  constructor(private http: Http) {}
+
+  get(url) {
+	let jwt = localStorage.getItem('id_token');
+	let authHeader = new Headers();
+	if (jwt) {
+		authHeader.append('Authorization', 'Bearer ' + jwt);
+	}
+
+	return this.http.get(url, {
+		headers: authHeader
+	});
+  }
+
+  post(url, data) {
+	let jwt = localStorage.getItem('id_token');
+	let authHeader = new Headers();
+	authHeader.append('Content-Type', 'application/json');
+	if (jwt) {
+		authHeader.append('Authorization', 'Bearer ' + jwt);
+	}
+
+	return this.http.post(url, data,
+		{ headers: authHeader });
+  }
+
+  put(url, data) {
+	  let jwt = localStorage.getItem('id_token');
+	  let authHeader = new Headers();
+	  authHeader.append('Content-Type', 'application/json');
+	  if (jwt) {
+		  authHeader.append('Authorization', 'Bearer ' + jwt);
+	  }
+
+	  return this.http.put(url, data,
+		  { headers: authHeader });
+  }
+
+}
